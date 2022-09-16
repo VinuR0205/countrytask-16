d5e4c2ae21a149ed3d3cc99e2315e41c83690e82
VinuR0205 <vinuramane2854@gmail.com>
2022-09-16 10:22:13 +0530

    work on contries task

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+let cl = console.log;
+
+const info = document.getElementById('info')
+let result = '';
+
+countries.forEach((contry) => {
+    result += `<div class="col">
+                <div class="card">
+                    <div class="imgcontainer">
+                    <img src=${contry.flag} alt="">
+                    </div>
+                    <h3 class="contryname">
+                         ${contry.name}
+                    </h3>
+                    <p>
+                        <b>capital</b>: ${contry.capital}
+                    </p>
+                    <p>
+                        <b>Languages</b>: ${contry.languages}
+                    </p>
+                    <p>
+                        <b>population</b>: ${contry.population}
+                    </p>
+                </div>
+            </div>`
+
+})
+info.innerHTML = result;
+
+
+
